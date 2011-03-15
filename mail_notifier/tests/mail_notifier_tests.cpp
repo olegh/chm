87ceae3e14f38015ec6bb43639c8d3024e9b94cd
olegh <snail.incm@gmail.com>
2011-03-16 00:15:13 +0300

    initial for mail_notifier

---
+++ mail_notifier/tests/mail_notifier_tests.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/cstdint.hpp>
+#include "math/fft.hpp"
+
+BOOST_AUTO_TEST_SUITE( mail_notifier_tests )
+
+BOOST_AUTO_TEST_CASE( notify_by_mail_test )
+{
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ mail_notifier/tests/tests.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE fft_test_suite
+#include <boost/test/unit_test.hpp>
