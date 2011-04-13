9409ba765ae3b353d101049f4c58ccb896b3762f
root <root@ubuntu.(none)>
2011-04-13 14:19:11 -0700

    update scripts examples

--- third_parties/hamachi.pl
+++ third_parties/hamachi.pl
@@ -1,30 +1,34 @@
 #!/usr/bin/perl
+
+system("hamachi logout");
+sleep 4;
+system("hamachi login");
+
 while( 1 )
 {
-  $counter_limit = 30;
-  $counter = 0;
+ $counter_limit = 30;
+ $counter = 0;
 
-  while( $counter <= $counter_limit )
-  {
-    system( "hamachi logout" );
-    sleep 4;
-  
-    $result = system( "hamachi login" );
-    if( 0 == $result )
-    {
-      last;
-    }
-  
-    $counter++;
-  }
+ while( $counter <= $counter_limit )
+ {
+   system( "hamachi logout" );
+   sleep 4;
+
+   $result = system( "hamachi login" );
+   if( 0 == $result )
+   {
+     last;
+   }
+
+   $counter++;
+ }
 
 
-  if( $counter > $counter_limit )
-  {
-    system( "reboot" );
-  }
+ if( $counter > $counter_limit )
+ {
+   system( "reboot" );
+ }
 
-  sleep 3600
-} 
-
+ sleep 3600
+}
 
